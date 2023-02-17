fe9a549f9d726db36888f67e2a59e9402b6ada3a
Mina <minassgerges@gmail.com>
2023-02-17 09:14:51 +0200

    add database and read and write to db

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
+//imports
 const express = require("express");
 const bodyParser = require("body-parser");
 const { render } = require("ejs");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
+//TODO:
+//remove old storage
+let workItems = [];
+
+//express setup
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-let items = [];
-let workItems = [];
+//mongoose setup
+mongoose.connect("mongodb://127.0.0.1:27017/todoDB");
 
+//schema setup
+const itemSchema = new mongoose.Schema({
+    name: String,
+    type: String
+});
+
+//models setup
+const todoItem = mongoose.model("todoItem", itemSchema);
+
+
+//routing
 app.get("/", function (req, res) {
-    let day = date();
-    res.render("list", {
-        listTitle: day,
-        items: items
+    const day = date();
+    const query = todoItem.find({type: "todo"}).select("name -_id");
+    query.exec(function(err, items) {
+        if (err) {
+            console.log("query error");
+        } else {
+            res.render("list", {
+                listTitle: day,
+                items: items
+            });
+        }
     })
 })
 
 app.get("/work", function (req, res) {
-    res.render("list", {
-        listTitle: "Work",
-        items: workItems
+    const query = todoItem.find({type: "work"}).select("name -_id");
+    query.exec(function(err, items) {
+        if (err) {
+            console.log("query error");
+        } else {
+            res.render("list", {
+                listTitle: "Work",
+                items: items
+            });
+        }
     })
 })
 
@@ -30,19 +62,23 @@ app.get("/about", function(req, res) {
     res.render("about");
 })
 
-
+//posts responses
 app.post("/", function (req, res) {
     console.log(req.body.list);
-    let item = req.body.newTodoItem;
-    if (req.body.list == "Work") {
-        workItems.push(item);
+    const listType = req.body.list === "work" ? "work" : "todo";
+    const item = {
+        name: req.body.newTodoItem,
+        type: listType
+    }
+    todoItem.create(item);
+    if (listType === "work") {
         res.redirect("/work");
     } else {
-        items.push(item);
         res.redirect("/");
     }
 })
 
+//server
 app.listen(3000, function () {
     console.log("server up");
 })
